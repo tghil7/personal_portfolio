41d55cf7b97bbffa4caa77bf6c31a6f49916ef63
Anicet Serge <tghil7@aol.com>
2017-10-27 13:02:25 -0500

    Changed about section's background picture, and modified its size and display. Added javascript function to roll through skills.

---
+++ js/portfolio.js
@@ -0,0 +1,35 @@
+$(function(){
+AOS.init();
+});
+
+$(window).on('load', function () {
+    AOS.refresh();
+});
+
+$("#aboutMe").ready(function(){
+var skillSpot = document.getElementById("skills");
+if ( skillSpot != null)
+{
+var skill = new Array(7);
+var curSkill = 0;
+skill[0] = "JQuery";
+skill[1] = "C#/ASP.NET Web Forms";
+skill[2] = "PHP";
+skill[3] = "Python";
+skill[4] = "Flask";
+skill[5] = "Knockout.js";
+skill[6] = "Javascript";
+function displaySkills(){
+    if (curSkill == 6){
+        curSkill = 0;
+		skillSpot.innerHTML = skill[curSkill]; 
+	}
+    else
+    {
+        curSkill++;
+         skillSpot.innerHTML = skill[curSkill];         
+    }
+   }     
+ setInterval(displaySkills, 2000);
+ }
+});
